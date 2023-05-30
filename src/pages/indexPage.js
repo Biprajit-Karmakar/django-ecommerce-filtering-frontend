c1547b31d9fc71d03776ef0ec35198e0676170fb
Biprajit-Karmakar <resetmibot@gmail.com>
2023-05-31 02:18:59 +0600

    fixing

--- src/pages/indexPage.js
+++ src/pages/indexPage.js
@@ -1,116 +1,112 @@
 import SideBarComponent from "../components/Sidebar.component";
-import HomeComponent from "../components/Home.component"
-import 'bootstrap/dist/css/bootstrap.css';
+import HomeComponent from "../components/Home.component";
+import "bootstrap/dist/css/bootstrap.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function IndexPageComponent(){
-    const [loader, setLoader] = useState(true);
-    
-    const [getProduct, setProduct] = useState([]);
+export default function IndexPageComponent() {
+  const [loader, setLoader] = useState(true);
 
-    //filtering & watch checkbox
-    const [selectBrand, setSelectBrand] = useState([]);
-    const [selectCategory, setSelectCategory] = useState([]);
-    const [selectType, setSelectType] = useState([]);
-    const [productUrl, setProductUrl] = useState('http://biprajit.pythonanywhere.com/products/');
-    let brandUrl = '';
-    let categoryUrl = '';
-    let typeUrl = '';
+  const [getProduct, setProduct] = useState([]);
 
-    useEffect(()=>{
-        axios.get(productUrl)
-        .then(function (response) {
-            setProduct(response.data);
-            setLoader(false);
-        })
-        .catch((error)=> {
-            // console.log(error);
-        });
-    },[productUrl]);
+  //filtering & watch checkbox
+  const [selectBrand, setSelectBrand] = useState([]);
+  const [selectCategory, setSelectCategory] = useState([]);
+  const [selectType, setSelectType] = useState([]);
+  const [productUrl, setProductUrl] = useState(
+    "http://biprajit.pythonanywhere.com/products/"
+  );
 
-    console.log('productUrl',productUrl);
-    useEffect(() => {
+  useEffect(() => {
+    axios.get(productUrl).then(function (response) {
+      setProduct(response.data);
+      setLoader(false);
+    });
+  }, [productUrl]);
 
-        buildRequestUrl();
-        // const requestUrl = buildRequestUrl();
-        
-    }, [selectBrand,selectCategory,selectType]);
-    
+  console.log("productUrl", productUrl);
+
+  useEffect(() => {
+    let brandUrl = "";
+    let categoryUrl = "";
+    let typeUrl = "";
 
     const buildRequestUrl = () => {
-        
-        if (selectBrand.length > 0) {
-            brandUrl += `${selectBrand.join(',')}`;
-        }
+      if (selectBrand.length > 0) {
+        brandUrl += `${selectBrand.join(",")}`;
+      }
 
-        if (selectCategory.length > 0) {
-            categoryUrl += `${selectCategory.join(',')}`;
-        }
+      if (selectCategory.length > 0) {
+        categoryUrl += `${selectCategory.join(",")}`;
+      }
 
-        if (selectType.length > 0) {
-            typeUrl += `${selectType.join(',')}`;
-        }
+      if (selectType.length > 0) {
+        typeUrl += `${selectType.join(",")}`;
+      }
 
-        return setProductUrl(`http://biprajit.pythonanywhere.com/products/?id=&slug=&brand=${brandUrl}&category=${categoryUrl}&type=${typeUrl}&warranty=`);
+      setProductUrl(
+        `http://biprajit.pythonanywhere.com/products/?id=&slug=&brand=${brandUrl}&category=${categoryUrl}&type=${typeUrl}&warranty=`
+      );
     };
+    buildRequestUrl();
+    // const requestUrl = buildRequestUrl();
+  }, [selectBrand, selectCategory, selectType]);
 
+  const handleBrandCheck = (event) => {
+    const { checked, value } = event.target;
+    setLoader(true);
+    setSelectBrand((prevSelectedBrands) => {
+      if (checked) {
+        return [...prevSelectedBrands, value];
+      } else {
+        return prevSelectedBrands.filter((brand) => brand !== value);
+      }
+    });
+  };
 
-    const handleBrandCheck = (event) => {
-        const {checked, value} = event.target;
-        setLoader(true);
-        setSelectBrand((prevSelectedBrands) => {
-            if (checked) {
-                return [...prevSelectedBrands, value];
-            } else {
-                return prevSelectedBrands.filter((brand) => brand !== value);
-            }
-        });
-        
-    };
+  const handleCategoryCheck = (event) => {
+    const { checked, value } = event.target;
+    setLoader(true);
+    setSelectCategory((prevSelectedBrands) => {
+      if (checked) {
+        return [...prevSelectedBrands, value];
+      } else {
+        return prevSelectedBrands.filter((category) => category !== value);
+      }
+    });
+  };
 
+  const handleTypeCheck = (event) => {
+    const { checked, value } = event.target;
+    setLoader(true);
+    setSelectType((prevSelectedBrands) => {
+      if (checked) {
+        return [...prevSelectedBrands, value];
+      } else {
+        return prevSelectedBrands.filter((type) => type !== value);
+      }
+    });
+  };
+  // console.log('getCheckValue', getCheckValue);
 
-    const handleCategoryCheck = (event) => {
-        const {checked, value} = event.target;
-        setLoader(true);
-        setSelectCategory((prevSelectedBrands) => {
-            if (checked) {
-                return [...prevSelectedBrands, value];
-            } else {
-                return prevSelectedBrands.filter((category) => category !== value);
-            }
-        });
-    }
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "15%", margin: "30px auto", padding: "15px 5px" }}>
+          <SideBarComponent
+            selectBrand={selectBrand}
+            selectCategory={selectCategory}
+            selectType={selectType}
+            handleBrandCheck={handleBrandCheck}
+            handleCategoryCheck={handleCategoryCheck}
+            handleTypeCheck={handleTypeCheck}
+          />
+        </div>
 
-    const handleTypeCheck = (event) => {
-        const {checked, value} = event.target;
-        setLoader(true);
-        setSelectType((prevSelectedBrands) => {
-            if (checked) {
-                return [...prevSelectedBrands, value];
-            } else {
-                return prevSelectedBrands.filter((type) => type !== value);
-            }
-        });
-    }
-    // console.log('getCheckValue', getCheckValue);
-
-    return (
-        <>
-            <div style={{ display: 'flex'}}>
-                
-                <div style={{ width:'15%', margin:'30px auto',  padding: '15px 5px'}}>
-                    <SideBarComponent selectBrand={selectBrand} selectCategory={selectCategory} selectType={selectType} handleBrandCheck={handleBrandCheck} handleCategoryCheck={handleCategoryCheck} handleTypeCheck={handleTypeCheck}/>
-                </div>
-
-                        
-
-                <div style={{ width:'85%'}}>
-                    <HomeComponent products={getProduct} loader={loader}/>
-                </div>
-
-            </div>
-            
-        </>
-    )
+        <div style={{ width: "85%" }}>
+          <HomeComponent products={getProduct} loader={loader} />
+        </div>
+      </div>
+    </>
+  );
 }
